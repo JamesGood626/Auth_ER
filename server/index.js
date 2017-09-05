ca096ad6bc20515747c3ace4ea8f3c0e1a083d0b
James <jamesgood626@gmail.com>
2017-09-05 10:46:13 -0700

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const router = require('./router')
 
+app.use(bodyParser.json({type: '*/*'}))
+router(app)
 
 const port = process.env.PORT || 5000
 app.listen(port)
