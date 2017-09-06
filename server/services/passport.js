c091f40a185a0a33178d731778aa6f0577605963
James <jamesgood626@gmail.com>
2017-09-06 00:24:24 -0700

    added passport secure route which requires valid jwt to access

---
+++ server/services/passport.js
@@ -0,0 +1,25 @@
+const passport = require('passport')
+const User = require('../models/user')
+const Config = require('../config')
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: Config.secret
+}
+
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(payload.sub, function(err, user) {
+    if(err) { return done(err, user) }
+
+    if(user) {
+      done(null, user)
+    }
+    else {
+      done(null, false)
+    }
+  })
+})
+
+passport.use(jwtLogin)
